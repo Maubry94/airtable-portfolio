19a270227e48a6bc0dcc5284fa8db64d6a327240
Maubry94 <matthieu.aubry94370@gmail.com>
2025-04-14 09:22:21 +0200

    start project detail page

--- src/domains/main/pages/ProjectDetailPage.vue
+++ src/domains/main/pages/ProjectDetailPage.vue
@@ -1,7 +1,21 @@
 <script setup lang="ts">
+import { useRouteParams } from "@/composables/useRouteParams";
+import { z } from "zod";
 
+const params = useRouteParams({
+	id: z.string(),
+});
 </script>
 
 <template>
-  Project detail page
+  <section class="min-h-screen-nh flex flex-col items-center justify-center">
+    <div class="text-center max-w-2xl">
+      <h1 class="text-5xl font-extrabold tracking-tight text-gray-900 mb-4">
+        Project Detail Page
+      </h1>
+      <p class="text-lg text-gray-600 mb-6">
+        Project ID: {{ params.id }}
+      </p>
+    </div>
+  </section>
 </template>
